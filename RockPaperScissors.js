5f42aba200cf7356ec41e3d086a000504d73c245
EvgeniGamanski <evgeny636@abv.bg>
2022-12-15 19:35:08 +0200

    Create RockPaperScissors.js

---
+++ RockPaperScissors.js
@@ -0,0 +1,52 @@
+function RockPaperScissors(input){
+//Type rock, paper or scissors in the input of the function at the end of the code :)
+let rock = "Rock";
+let paper = "Paper";
+let scissors = "Scissors"; 
+
+let playerTurn=input;  
+
+let computerTurn;
+
+if (playerTurn == "rock" || playerTurn == "Rock") {
+    playerTurn = rock;
+}
+else if (playerTurn == "paper" || playerTurn == "Paper") {
+    playerTurn = paper;
+}
+else if (playerTurn == "scissors" || playerTurn == "Scissors") {
+    playerTurn = scissors;
+}
+else {
+    console.log("The input is invalid! Try putting a valid one!");
+    return;
+}
+console.log("You choose " + playerTurn);
+
+let randomNumber = Math.floor(Math.random() * 3) + 1;
+
+switch (randomNumber) {
+    case 1:
+        computerTurn = rock;
+        break;
+    case 2:
+        computerTurn = paper;
+        break;
+    case 3:
+        computerTurn = scissors;
+        break;
+}
+
+console.log(`The computer chooses ${computerTurn}`);
+
+if ((playerTurn === rock && computerTurn === scissors) || (playerTurn === paper && computerTurn === rock) || (playerTurn === scissors && computerTurn === paper)) {
+    console.log("You win!");
+}
+else if ((playerTurn === rock && computerTurn === paper) || (playerTurn === paper && computerTurn === scissors) || (playerTurn === scissors && computerTurn === rock)) {
+    console.log("You lose!");
+}
+else if ((playerTurn === rock && computerTurn === rock) || (playerTurn === paper && computerTurn === paper) || (playerTurn === scissors && computerTurn === scissors)) {
+    console.log("Draw!");
+}
+}
+RockPaperScissors("paper"); 
